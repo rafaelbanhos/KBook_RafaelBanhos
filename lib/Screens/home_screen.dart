df68e301f1a58f5dbc6d36bfd0af01850b1cf75d
rafaelbanhos <rafaelbanhos@gmail.com>
2020-08-26 10:19:08 +0100

    get data from api

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,17 +10,100 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _search;
+  int _offset = 0;
+
+  Future<Map> _getBooks() async {
+    http.Response response;
+
+    response = await http.get(
+        "https://www.googleapis.com/books/v1/volumes?q=flutter&maxResults=20&startIndex=0");
+    return json.decode(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(25, 179, 190, 1),
-        title: Text('Kentra Challenge'),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(25, 179, 190, 1),
+          title: Text('Kentra Challenge'),
+          centerTitle: true,
+        ),
+        body: FutureBuilder(
+          future: _getBooks(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+              case ConnectionState.none:
+                return Container(
+                  width: 200.0,
+                  height: 200.0,
+                  alignment: Alignment.center,
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                    strokeWidth: 5.0,
+                  ),
+                );
+              default:
+                if (snapshot.hasError)
+                  return Container();
+                else
+                  return _createBooksTable(context, snapshot);
+            }
+          },
+        ));
+  }
+
+  int _getCount(List data) {
+    if (_search == null) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
+  Widget _createBooksTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+        itemCount: _getCount(snapshot.data["items"]),
+        itemBuilder: (context, index) {
+          if (index < snapshot.data["items"].length)
+            return GestureDetector(
+              child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: snapshot.data["items"][index]["volumeInfo"]["imageLinks"]
+                    ["smallThumbnail"],
+                height: 300.0,
+                fit: BoxFit.cover,
+              ),
+              onTap: () {},
+            );
+          else
+            return Container(
+              child: GestureDetector(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 70.0,
+                    ),
+                    Text(
+                      "Carregar mais...",
+                      style: TextStyle(color: Colors.white, fontSize: 22.0),
+                    )
+                  ],
+                ),
+                onTap: () {
+                  setState(() {
+                    _offset += 19;
+                  });
+                },
+              ),
+            );
+        });
   }
 }
---
+++ lib/Services/config.dart
@@ -0,0 +1,6 @@
+class Config {
+  static const String apiKey = '-';
+  static const String language = 'pt';
+  static const String region = 'pt';
+  static const String apiUrl = '';
+}
